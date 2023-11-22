9226ff05ce9abc8f77d35a49d6851a71c48eb5ef
Ramiro Magno <rmagno@pattern.institute>
2023-11-22 13:37:10 +0000

    first commit

---
+++ R/add.R
@@ -0,0 +1,11 @@
+#' Add two vectors
+#'
+#' @param x A numeric vector.
+#' @param y A numeric vector.
+#'
+#' @returns A numeric vector with the summation result.
+#'
+#'
+add <- function(x, y) {
+  x + y
+}
